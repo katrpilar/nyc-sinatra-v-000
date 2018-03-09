e9157f74d5a3725bfe368ded68507de53e30d356
Katrina Brinson <kbrinso@gmail.com>
2018-03-09 19:43:20 +0000

    new-fig controller 2 passing

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,7 +10,17 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(name: params[:name])
+    @figure = Figure.create(name: params[:figure][:name])
+    #binding.pry
+
+    if !params[:title][:name].empty?
+      @figure.title_ids = params[:figure][:title_ids]
+      @title = Title.create(name: params[:title][:name])
+      @figure.title_ids << @title.id
+    else
+      @figure.title_ids = params[:figure][:title_ids]
+    end
+
     @figure.save
     redirect to "/figures/#{@figure.id}"
   end
